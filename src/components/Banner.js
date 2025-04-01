46f4ed0221c9881a1dc0163166defa6a21027411
PRATIK SARKAR <pratiksarkarofficial004@gmail.com>
2025-04-01 19:55:51 +0530

    first

---
+++ src/components/Banner.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import Lottie from 'react-lottie';
+import animationData from '../assets/lottie/Animation - 1743514225429.json'; // Path to the Lottie JSON file
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Computer Science Student.", "ML Enthusiast.", "Web Developer." ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  const defaultOptions = {
+    loop: true, 
+    autoplay: true, 
+    animationData: animationData, 
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice', // Keeps aspect ratio
+    },
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>{`Hi! I'm Pratik`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Computer Science Student.", "ML Enthusiast.", "Web Developer." ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Hey there! I'm Pratik, a passionate and curious tech enthusiast driven by innovation and problem-solving. From crafting smart solutions with Python to diving into the world of AI and web development, I thrive on turning ideas into impactful digital experiences. Whether it’s building data-driven applications, exploring machine learning, or designing sleek user interfaces, I’m all about blending creativity with code. Let’s connect and create something awesome together!</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                </div>
+              }
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <Lottie options={defaultOptions} height={400} width={400} />
+                </div>
+              }
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
